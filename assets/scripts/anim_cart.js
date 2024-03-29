a971b336ae38a691b15df22fe4c6301b22f2a001
JoseH <jose.airton.junior@gmail.com>
2024-03-29 20:25:34 -0300

    Acerto no menu e definicoes Dark Mode

--- assets/scripts/anim_cart.js
+++ assets/scripts/anim_cart.js
@@ -1,13 +1,18 @@
+/*Função para quando o mouse passar por cima do cartão*/
+
 function handleMouseEnter() {
     this.classList.add('chovered');
     document.body.id = `${this.id}hovered`;
 }
+
+/*Função para quando o mouse sair de cima do cartão */
 
 function handleMouseLeave() {
     this.classList.remove('chovered')
     document.body.id='';
 }
 
+/*Função para adicionar as classes definidas anteriormente ao elemento */
 function addEventListenersToCards() {
     const cardElements = document.getElementsByClassName('cartao');
 
@@ -18,4 +23,5 @@ function addEventListenersToCards() {
     }
 }
 
+/*método para chamar as funções definidas*/
 document.addEventListener("DOMContentLoaded", addEventListenersToCards, false)
