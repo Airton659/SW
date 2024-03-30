f5bac0e0f052ad019737548cadc2592b138f27b4
JoseH <jose.airton.junior@gmail.com>
2024-03-30 12:55:16 -0300

    salvando preferencia do tema

--- assets/scripts/dm.js
+++ assets/scripts/dm.js
@@ -1,6 +1,21 @@
-const changeThemeBtn = document.querySelector("#change-theme")
+const changeThemeBtn = document.querySelector("#change-theme");
 const root = document.querySelector("html");
+const themeSystem = localStorage.getItem('themeSystem') || 'light'
 
+
+/*Função para alterar o tema*/
+function toggleDarkMode (theme) {
+    root.classList.toggle("dark-mode");   
+}
+
+/*carregando o tema*/
 changeThemeBtn.addEventListener("change",function() {
-    root.classList.toggle("dark-mode");
+    let oldTheme = localStorage.getItem('themeSystem') || 'light'
+    let newTheme = oldTheme == "light" ? "dark" : "light"
+
+    localStorage.setItem("themeSystem",newTheme)
+    toggleDarkMode(newTheme);
+// 
 })
+
+toggleDarkMode(themeSystem)
